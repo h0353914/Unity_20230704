4fd78eafcdd625b80ca0ee3491f74e33b782c7ab
h0353914 <45159732+h0353914@users.noreply.github.com>
2023-07-04 14:08:03 +0800

    Update 新增等級

--- Assets/A.cs
+++ Assets/A.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class A : MonoBehaviour
 {
+    int lv = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
