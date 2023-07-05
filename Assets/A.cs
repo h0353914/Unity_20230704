38149d278a7550282fec88f1c990f2e994af13cf
h0353914 <45159732+h0353914@users.noreply.github.com>
2023-07-05 16:26:41 +0800

    Update 改一半

--- Assets/A.cs
+++ Assets/A.cs
@@ -6,16 +6,26 @@ public class A : MonoBehaviour
 {
     float x = 0;
     int lv = 1;
+    private Rigidbody2D rBody;
+
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        x += Input.GetAxis("Horizontal") * Time.deltaTime * 10;
-        transform.position = new Vector2(x, -2);
+
+        //Vector2 vector=Vector2.zero;
+        //vector.x = Input.GetAxis("Horizontal") * Time.deltaTime * 10;
+        rBody.velocityX = Input.GetAxis("Horizontal") * Time.deltaTime * 10;
+
+
+
+
+        //transform.position = vector;
     }
 }
