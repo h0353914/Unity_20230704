865f33c9d46d2fc730b71b29c27ecbc09265d577
h0353914 <45159732+h0353914@users.noreply.github.com>
2023-07-05 09:28:07 +0800

    Update 新增 豬 ㄎand 樹

--- Assets/A.cs
+++ Assets/A.cs
@@ -1,9 +1,10 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class A : MonoBehaviour
 {
+    float x = 0;
     int lv = 1;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +15,7 @@ public class A : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        x += Input.GetAxis("Horizontal") * Time.deltaTime * 10;
+        transform.position = new Vector2(x, -2);
     }
 }
